2239b174c5af01a19c62705d940b24b03874e7b1
NinaVisochina <114816977+NinaVisochina@users.noreply.github.com>
2024-10-02 17:21:25 +0300

    Init

---
+++ WebSimba/Mapper/AppMapperProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using WebSimba.Data.Entities;
+using WebSimba.Models.Category;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace WebSimba.Mapper
+{
+    public class AppMapperProfile : Profile
+    {
+        public AppMapperProfile()
+        {
+            //Початок
+            CreateMap<CategoryEntity, CategoryItemModel>()
+                .ForMember(x => x.ImagePath, opt => opt.MapFrom(x =>
+                    string.IsNullOrEmpty(x.Image) ? "/images/noimage.jpg" : $"/images/{x.Image}"));
+
+            CreateMap<CategoryCreateModel, CategoryEntity>()
+                .ForMember(x => x.Image, opt => opt.Ignore());
+        }
+    }
+}
+
+
